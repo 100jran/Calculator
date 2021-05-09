3e32f4f741f63b84f7b51e89642256fe185bae3b
100jran <100jran@gmail.com>
2021-05-09 22:50:57 +0900

    2.사칙연산-값 2개 입력받기 추가

--- src/baek/Calculator/Calculator.java
+++ src/baek/Calculator/Calculator.java
@@ -1,13 +1,30 @@
 package baek.Calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
 
 	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
 		
-		System.out.printf("더하기 : %d\n", 2 + 1);
-		System.out.printf("빼기 : %d\n", 2 - 1);
-		System.out.printf("곱하기 : %d\n", 2 * 1);
-		System.out.printf("나누기 : %d\n", 2 / 1);
+		System.out.printf("두 수를 입력하세요.\n\n");
 		
+		System.out.println("첫번째 입력 값을 입력하세요.");
+		System.out.print("FIRST NUM> ");
+		int numOne = scan.nextInt();
+		System.out.println();
+		
+		System.out.println("두번째 입력 값을 입력하세요.");
+		System.out.print("SECOND NUM> ");
+		int numTwo = scan.nextInt();
+		System.out.println();
+
+		System.out.println("사칙연산 결과");
+		System.out.printf("+ : %d + %d = %d\n", numOne, numTwo, numOne + numTwo);
+		System.out.printf("- : %d - %d = %d\n", numOne, numTwo, numOne - numTwo);
+		System.out.printf("× : %d × %d = %d\n", numOne, numTwo, numOne * numTwo);
+		System.out.printf("÷ : %d ÷ %d = %d\n", numOne, numTwo, numOne / numTwo);
+
 	}
 }
