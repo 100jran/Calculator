a5b162bd0d5e041ab49da08a28f7a996a6f079ce
100jran <100jran@gmail.com>
2021-05-09 22:33:38 +0900

    1.사칙연산-printf

---
+++ src/baek/Calculator/Calculator.java
@@ -0,0 +1,13 @@
+package baek.Calculator;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("더하기 : %d\n", 2 + 1);
+		System.out.printf("빼기 : %d\n", 2 - 1);
+		System.out.printf("곱하기 : %d\n", 2 * 1);
+		System.out.printf("나누기 : %d\n", 2 / 1);
+		
+	}
+}
